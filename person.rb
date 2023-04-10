9e2bd2ea3574cddae81caf48e5747c524392d3c1
Lancelot-SO <fsowah001@gmail.com>
2023-04-10 12:46:49 +0000

    Add teacher file

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ class Person
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
-    @age = _age
+    @age = age
     @parent_permission = parent_permission
   end
 
