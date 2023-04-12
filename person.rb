c79010ab287d1834b8a0f91d5538dcb449a37d39
Lancelot-SO <fsowah001@gmail.com>
2023-04-12 16:22:48 +0000

    implemented changes requested

--- person.rb
+++ person.rb
@@ -1,28 +1,64 @@
 require_relative 'nameable'
+
 class Person < Nameable
+
   def initialize(age, name = 'Unknown', parent_permission: true)
+
     @id = Random.rand(1..1000)
+
     @name = name
+
     @age = age
+
     @parent_permission = parent_permission
+
     @rental = []
+
     super()
+
   end
+
+
 
   def correct_name
+
     name
+
   end
 
+
+
+  def add_rental(book, date)
+
+    Rental.new(date, book, self)
+
+  end
+
+
+
   attr_accessor :name, :age, :rentals
+
   attr_reader :id
+
+
 
   private
 
+
+
   def of_age?
+
     @age >= 18
+
   end
 
+
+
   def can_use_services?
+
     of_age? || @parent_permission
+
   end
+
 end
+
