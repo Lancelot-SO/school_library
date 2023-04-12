482e2429adfb092675518630007363c2ac53d87d
Lancelot-SO <fsowah001@gmail.com>
2023-04-12 16:27:48 +0000

    fixed linters error

--- person.rb
+++ person.rb
@@ -1,17 +1,11 @@
 require_relative 'nameable'
-
 class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
-
     @name = name
-
     @age = age
-
     @parent_permission = parent_permission
-
     @rental = []
-
     super()
   end
 
@@ -23,17 +17,10 @@ class Person < Nameable
     Rental.new(date, book, self)
   end
 
-
-
   attr_accessor :name, :age, :rentals
-
   attr_reader :id
 
-
-
   private
-
-
 
   def of_age?
     @age >= 18
